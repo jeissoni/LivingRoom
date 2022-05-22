ae5e4fe227e5a243824d713484bf2af65a71159c
jeissoni_22 <jeissoni_22@hotmail.com>
2022-05-22 15:30:26 -0500

    add files for the architecure

---
+++ LivingRoom.Repositoy/DataContext/LivingRoomContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivingRoom.Repositoy.DataContext
+{
+    public class LivingRoomContext : DbContext
+    {
+        public LivingRoomContext(
+            DbContextOptions<LivingRoomContext> options) : base(options) { }
+        
+    }
+}
