44feb2222e39f982cf9a506d945d0da03e31325d
jeissoni <jeissoni_22@hotmail.com>
2022-05-23 17:20:43 -0500

    add cdbcontext factory repository

---
+++ LivingRoom.Repositoy/LivingRoomContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using LivingRoom.Repositoy.DataContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivingRoom.Repositoy
+{
+    internal class LivingRoomContextFactory : IDesignTimeDbContextFactory<LivingRoomContext>
+    {
+        public LivingRoomContext CreateDbContext(string[] args)
+        {
+            var OptionBuilder = new DbContextOptionsBuilder<LivingRoomContext>();
+
+            OptionBuilder.UseNpgsql("Host=localhost;Database=pruebas;Username=UsuSistema;Password=Welcome101!");
+
+            return new LivingRoomContext(OptionBuilder.Options);    
+        }
+    }
+}
