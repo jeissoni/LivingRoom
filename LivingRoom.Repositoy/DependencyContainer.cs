77a6ea4156fb6c5b0c9061a130f01a00e5630922
jeissoni <jeissoni_22@hotmail.com>
2022-05-26 18:01:17 -0500

    add files for repository pattern

---
+++ LivingRoom.Repositoy/DependencyContainer.cs
@@ -0,0 +1,27 @@
+﻿using LivingRoom.Entities.Interfaces;
+using LivingRoom.Repositoy.Models;
+using LivingRoom.Repositoy.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivingRoom.Repositoy
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddRepositories(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<bdtosahdContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("LivingRoom")));
+
+            services.AddScoped<IUsuarioRepository, UserRepository>();
+        }
+    }
+}
---
+++ LivingRoom.Repositoy/Repositories/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using LivingRoom.Entities.DTO;
+using LivingRoom.Entities.Interfaces;
+using LivingRoom.Repositoy.DataContext;
+using LivingRoom.Repositoy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivingRoom.Repositoy.Repositories
+{
+    public class UserRepository : IUsuarioRepository
+    {
+        readonly bdtosahdContext context;
+
+        public UserRepository(bdtosahdContext _context) 
+        {
+            context = _context;
+        }
+        public void CreateUser(UsuarioDTO usuario)
+        {
+            context.Add(usuario);
+        }
+
+        public IEnumerable<TUsuarios> GetAll()
+        {
+            return context.TUsuarios;
+        }
+    }
+}
