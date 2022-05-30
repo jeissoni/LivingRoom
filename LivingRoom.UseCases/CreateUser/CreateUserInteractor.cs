cf594216ed97061a548f93a719116e570868e6e0
jeissoni_22 <jeissoni_22@hotmail.com>
2022-05-30 14:59:57 -0500

    add new proyect

---
+++ LivingRoom.UseCases/CreateUser/CreateUserInteractor.cs
@@ -0,0 +1,44 @@
+﻿using LivingRoom.DTOs;
+using LivingRoom.Entities.Interfaces;
+using LivingRoom.Entities.POCOs;
+using LivingRoom.UseCasesPort;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivingRoom.UseCases.CreateUser
+{
+    public class CreateUserInteractor : ICreateUserInputPort
+    {
+        readonly IUsuarioRepository repository;
+        readonly IUnitOfWork unitOfWork;
+        readonly ICreateUserOutPutPort outPutPort;
+
+        public CreateUserInteractor(
+            IUsuarioRepository _repository,
+            IUnitOfWork _unitOfWork,
+            ICreateUserOutPutPort _outPutPort)            
+        {
+            repository = _repository;
+            unitOfWork = _unitOfWork;
+            outPutPort = _outPutPort;
+        }
+        public async Task Handle(CreateUserDTO user)
+        {
+            User newUser = new User { 
+                Name = user.UserName
+            };
+
+            repository.CreateUser(newUser);
+            await unitOfWork.SaveChanges();
+            await outPutPort.Handle(
+                new UserDTO
+                {
+                    Id = newUser.Id,
+                    Name = user.UserName
+                });
+        }
+    }
+}
---
+++ LivingRoom.UseCasesPort/ICreateUserInputPort.cs
@@ -0,0 +1,11 @@
+﻿using LivingRoom.DTOs;
+
+namespace LivingRoom.UseCasesPort
+{
+    //recibir desde un controlador un DTO 
+    //datos de entrada del caso de uso
+    public interface ICreateUserInputPort
+    {
+        Task Handle(CreateUserDTO user);
+    }
+}
