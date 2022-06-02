7f3616e08bb674e1563cfeed9b06b1e94c3e0415
jeissoni <jeissoni_22@hotmail.com>
2022-06-02 18:32:11 -0500

    other changes

--- LivingRoom.Repositoy/DataContext/LivingRoomContext.cs
+++ LivingRoom.Repositoy/DataContext/LivingRoomContext.cs
@@ -1,10 +1,5 @@
 ﻿using LivingRoom.Entities.POCOs;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LivingRoom.Repositoy.DataContext
 {
--- LivingRoom.Repositoy/LivingRoomContextFactory.cs
+++ LivingRoom.Repositoy/LivingRoomContextFactory.cs
@@ -1,11 +1,6 @@
 ﻿using LivingRoom.Repositoy.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LivingRoom.Repositoy
 {
