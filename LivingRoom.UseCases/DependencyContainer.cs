c1d7c9e1404b6722517d9023902c973b56396d6a
jeissoni <jeissoni_22@hotmail.com>
2022-06-02 18:31:49 -0500

    add container for the services

---
+++ LivingRoom.UseCases/DependencyContainer.cs
@@ -0,0 +1,32 @@
+﻿using LivingRoom.UseCases.CreateUser;
+using LivingRoom.UseCases.GetAllProducts;
+using LivingRoom.UseCasesPort;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivingRoom.UseCases
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddUseCasesServices(
+            this IServiceCollection services)
+        {
+
+            // cuando me lo pidan regreso la intancia, 
+            // por eso se usa 'Transient'
+
+            //en los contexto, durante toda la peticion
+            //cuando me pidan  un contexto, regresar el mismo contexto 
+            //por  eso se usa como 'Scoped'
+            services.AddTransient<ICreateUserInputPort, CreateUserInteractor>();
+
+            services.AddTransient<IGetAllUserInputPort, GetAllUserInteractor>();
+
+            return services;
+        }
+    }
+}
