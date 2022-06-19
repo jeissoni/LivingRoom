c5b2e49f71a1fd706a0eacb5a39ff68b1e8c4b1a
jeissoni_22 <jeissoni_22@hotmail.com>
2022-06-18 19:13:42 -0500

    using secret

--- LivingRoom.Repositoy/LivingRoomContextFactory.cs
+++ LivingRoom.Repositoy/LivingRoomContextFactory.cs
@@ -1,20 +1,30 @@
 ﻿using LivingRoom.Repositoy.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace LivingRoom.Repositoy
 {
     //fabrica de contexto
-    class LivingRoomContextFactory : 
+    public class LivingRoomContextFactory : 
         IDesignTimeDbContextFactory<LivingRoomContext>
     {
+        private readonly IConfiguration _configuration;
+
+        public LivingRoomContextFactory(IConfiguration configuration) 
+        {
+            _configuration = configuration;
+        }
         public LivingRoomContext CreateDbContext(string[] args)
         {
             var OptionBuilder = 
                 new DbContextOptionsBuilder<LivingRoomContext>();
 
             OptionBuilder.UseNpgsql
-                ("Host=tuffi.db.elephantsql.com;Database=bdtosahd;Username=bdtosahd;Password=exF7SDHWA1Km8NVFWUGzhV0FM4X1Di72");
+                ($"Host={_configuration["Host"]};" +
+                $"Database={_configuration["Database"]};" +
+                $"Username={_configuration["Username"]};" +
+                $"Password= {_configuration["Password"]}");
 
             return new LivingRoomContext(OptionBuilder.Options);    
         }
