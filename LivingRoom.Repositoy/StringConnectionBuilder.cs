fea4d744bf59d3d27e0eea0534fb375c96cf5106
jeissoni_22 <jeissoni_22@hotmail.com>
2022-06-26 09:25:00 -0500

    add file for stringconnection

---
+++ LivingRoom.Repositoy/StringConnectionBuilder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace LivingRoom.Repositoy
+{
+    public class StringConnectionBuilder
+    {
+        
+        private readonly IConfiguration _configuration;
+
+        public StringConnectionBuilder(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string ConnectionString() 
+        {
+            SqlConnectionStringBuilder
+                connectionStringBuilder = new();
+
+            connectionStringBuilder.DataSource = _configuration["Host"];
+            connectionStringBuilder.InitialCatalog = _configuration["Database"];
+            connectionStringBuilder.UserID = _configuration["Username"];
+            connectionStringBuilder.Password = _configuration["Password"];
+
+            return connectionStringBuilder.ConnectionString;
+        }
+    }
+}
