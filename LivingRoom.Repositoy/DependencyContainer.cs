b747a1417062837a95be28e5fccc9880eeddc359
jeissoni_22 <jeissoni_22@hotmail.com>
2022-07-17 20:08:34 -0500

    add conection string from secret

--- LivingRoom.Repositoy/DependencyContainer.cs
+++ LivingRoom.Repositoy/DependencyContainer.cs
@@ -4,19 +4,35 @@ using LivingRoom.Repositoy.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Data.SqlClient;
 
 namespace LivingRoom.Repositoy
 {
     public static class DependencyContainer
     {
+
         //para poder inyectar los servicios
         public static IServiceCollection AddRepositories(
             this IServiceCollection services,
             IConfiguration configuration)
         {
+            
+            var OptionBuilder =
+               new DbContextOptionsBuilder<LivingRoomContext>();
+
+            var connectionString = $"Host={configuration["Host"]};" +
+                $"Database={configuration["Database"]};" +
+                $"Username={configuration["User"]};" +
+                $"Password={configuration["Password"]}";
+
             services.AddDbContext<LivingRoomContext>(options =>
-            options.UseNpgsql(
-                configuration.GetConnectionString("LivingRoom")));
+                        options.UseNpgsql(
+                            connectionString)
+                            //configuration.GetConnectionString("LivingRoom"))
+            );
+
+            services.AddDbContext<LivingRoomContext>(options =>
+            options.UseNpgsql(connectionString));
 
             services.AddScoped<IUsuarioRepository, UserRepository>();
 
