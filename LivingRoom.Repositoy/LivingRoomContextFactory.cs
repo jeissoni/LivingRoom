0a44290a2bac9f7722fc8f60d0e665a2a30f2a7f
jeissoni_22 <jeissoni_22@hotmail.com>
2022-07-19 16:04:26 -0500

    add new changes

--- LivingRoom.Repositoy/LivingRoomContextFactory.cs
+++ LivingRoom.Repositoy/LivingRoomContextFactory.cs
@@ -2,29 +2,36 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
+using Npgsql;
 
 namespace LivingRoom.Repositoy
 {
     //fabrica de contexto
+    // tiempo de diseño, no se crean en tiempo de ejecucion 
     public class LivingRoomContextFactory : 
         IDesignTimeDbContextFactory<LivingRoomContext>
     {
-
-        //private readonly IConfiguration _configuration;
-        private readonly StringConnectionBuilder _stringConnectionBuilder;
-
-        public LivingRoomContextFactory(IConfiguration configuration) 
-        {
-            _stringConnectionBuilder = new StringConnectionBuilder(configuration);
-        }
+              
         public LivingRoomContext CreateDbContext(string[] args)
-        {
-            var OptionBuilder = 
-                new DbContextOptionsBuilder<LivingRoomContext>();                       
+        {          
 
-            OptionBuilder.UseNpgsql(_stringConnectionBuilder.ConnectionString());
-                        
+            var OptionBuilder =
+             new DbContextOptionsBuilder<LivingRoomContext>();
+
+            OptionBuilder.UseNpgsql
+                 ("Host=tuffi.db.elephantsql.com;Database=bdtosahd;Username=bdtosahd;Password=exF7SDHWA1Km8NVFWUGzhV0FM4X1Di72");
+                                
             return new LivingRoomContext(OptionBuilder.Options);    
-        }
+        }       
+
     }
 }
+
+//1.
+// add - migration Migration19072022 - p LivingRoom.Repositoy - c LivingRoomContext - o Migrations - s LivingRoom.Repositoy
+// Add-Migration [-Name] <String> [-OutputDir <String>] [-Context <String>] [-Project <String>][-StartupProject<String>][<CommonParameters>]
+// add-migration Migration19072022 -p LivingRoom.Repositoy -c LivingRoomContext -o Migrations -s LivingRoom.Repositoy
+
+
+//2. 
+//update-database Migration19072022 -p LivingRoom.Repositoy -s LivingRoom.Repositoy
