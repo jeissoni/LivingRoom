7006298bcd1ab5324dbc3af895438e32c21d10b1
jeissoni_22 <jeissoni_22@hotmail.com>
2022-07-28 18:01:59 -0500

    add new migrations

---
+++ LivingRoom.Repositoy/Migrations/20220728225654_Migration28072022.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace LivingRoom.Repositoy.Migrations
+{
+    public partial class Migration28072022 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Occupations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Occupations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SchoolGrade",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SchoolGrade", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Segmentations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Segmentations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    CountryForeignKey = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cities_Countries_CountryForeignKey",
+                        column: x => x.CountryForeignKey,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LivingGroups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CityId = table.Column<int>(type: "integer", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    MinAge = table.Column<int>(type: "integer", nullable: false),
+                    MaxAge = table.Column<int>(type: "integer", nullable: false),
+                    Day = table.Column<int>(type: "integer", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LivingGroups", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LivingGroups_Cities_CityId",
+                        column: x => x.CityId,
+                        principalTable: "Cities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserAddresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    Neighborhood = table.Column<string>(type: "text", nullable: false),
+                    CityId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAddresses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserAddresses_Cities_CityId",
+                        column: x => x.CityId,
+                        principalTable: "Cities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    IdUser = table.Column<int>(type: "integer", nullable: true),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Gender = table.Column<int>(type: "integer", nullable: false),
+                    OccupationForeignKey = table.Column<int>(type: "integer", nullable: false),
+                    SegmentationForeignKey = table.Column<int>(type: "integer", nullable: false),
+                    SchoolGradeForeignKey = table.Column<int>(type: "integer", nullable: false),
+                    CellPhoneNumber = table.Column<int>(type: "integer", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Leader = table.Column<bool>(type: "boolean", nullable: false),
+                    Support = table.Column<bool>(type: "boolean", nullable: false),
+                    Attendee = table.Column<bool>(type: "boolean", nullable: false),
+                    Sons = table.Column<bool>(type: "boolean", nullable: false),
+                    NumberOfChildren = table.Column<int>(type: "integer", nullable: false),
+                    DaysAvailable = table.Column<int[]>(type: "integer[]", nullable: false),
+                    UserAddressForeignKey = table.Column<int>(type: "integer", nullable: false),
+                    LivingGroupForeignKey = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_LivingGroups_LivingGroupForeignKey",
+                        column: x => x.LivingGroupForeignKey,
+                        principalTable: "LivingGroups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Users_Occupations_OccupationForeignKey",
+                        column: x => x.OccupationForeignKey,
+                        principalTable: "Occupations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Users_SchoolGrade_SchoolGradeForeignKey",
+                        column: x => x.SchoolGradeForeignKey,
+                        principalTable: "SchoolGrade",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Users_Segmentations_SegmentationForeignKey",
+                        column: x => x.SegmentationForeignKey,
+                        principalTable: "Segmentations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Users_UserAddresses_UserAddressForeignKey",
+                        column: x => x.UserAddressForeignKey,
+                        principalTable: "UserAddresses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_CountryForeignKey",
+                table: "Cities",
+                column: "CountryForeignKey");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LivingGroups_CityId",
+                table: "LivingGroups",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAddresses_CityId",
+                table: "UserAddresses",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_LivingGroupForeignKey",
+                table: "Users",
+                column: "LivingGroupForeignKey");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_OccupationForeignKey",
+                table: "Users",
+                column: "OccupationForeignKey",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_SchoolGradeForeignKey",
+                table: "Users",
+                column: "SchoolGradeForeignKey",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_SegmentationForeignKey",
+                table: "Users",
+                column: "SegmentationForeignKey",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserAddressForeignKey",
+                table: "Users",
+                column: "UserAddressForeignKey");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "LivingGroups");
+
+            migrationBuilder.DropTable(
+                name: "Occupations");
+
+            migrationBuilder.DropTable(
+                name: "SchoolGrade");
+
+            migrationBuilder.DropTable(
+                name: "Segmentations");
+
+            migrationBuilder.DropTable(
+                name: "UserAddresses");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
